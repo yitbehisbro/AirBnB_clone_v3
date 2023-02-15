52cad8bd075b0fa310a32492258d938517e2c0be
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2023-02-15 13:22:14 +0300

    Updates to the file as per task 5

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,6 +4,8 @@ from models import storage
 from api.v1.views import app_views
 from flask import Flask
 from os import getenv
+from flask import make_response
+from flask import jsonify
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -14,11 +16,22 @@ def teardown_db(exception):
     """ Tear down method """
     storage.close()
 
+@app.errorhandler(404)
+def not_found(error):
+    """ Not found handler """
+    return make_response(jsonify({"error": "Not found"}), 404)
+
 
 if __name__ == "__main__":
     HBNB_API_HOST = getenv('HBNB_API_HOST')
     HBNB_API_PORT = getenv('HBNB_API_PORT')
 
     if HBNB_API_HOST and HBNB_API_PORT:
+        """
+        host = '0.0.0.0' if not HBNB_API_HOST else HBNB_API_HOST
+        port = 5000 if not HBNB_API_PORT else HBNB_API_PORT
+
+        app.run(host=host, port=port, threaded=True)
+        """
         app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
     app.run(host='0.0.0.0', port=5000, threaded=True)
