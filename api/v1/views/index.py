703ddf911f2dddd87cc85ad5919b422de140ecef
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2023-02-15 13:14:17 +0300

    Updates regarding the task number 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,27 @@
 """ Index file module """
 from api.v1.views.__init__ import app_views
 from flask import jsonify
+from models.state import State
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.user import User
+from models import storage
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """ Displays the status """
     stat = {"status": "OK"}
     return jsonify(stat)
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    """Displays the status """
+    dic = {"amenities": storage.count(Amenity),
+            "cities": storage.count(City),
+            "places": storage.count(Place),
+            "reviews": storage.count(Review),
+            "states": storage.count(State),
+            "users": storage.count(User)}
+    return jsonify(dic)
