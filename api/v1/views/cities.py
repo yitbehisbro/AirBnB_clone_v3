97736efd26b2ea2aad382e47dbbc9be5e8266109
yitbehisbro <yitbarekw@hu.edu.et>
2023-02-16 15:55:20 +0300

    Content added on cities

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -52,9 +52,6 @@ def post_request1(state_id):
         abort(400, 'Not a JSON')
     if 'name' not in kwargs:
         abort(400, 'Missing name')
-    states = kwargs['state_id']
-    if states not in storage.get(State.__name__, state_id):
-        abort(404)
     get_city = City(**kwargs)
     setattr(get_city, 'state_id', state_id)
     storage.new(get_city)
