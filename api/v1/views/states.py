a8b1e72657f60a48609167b8a49a97adb4fc4ece
yitbehisbro <yitbarekw@hu.edu.et>
2023-02-16 14:58:30 +0300

    Some fixes on states

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -34,7 +34,7 @@ def delete_by_id(state_id):
         abort(404)
     get_state.delete()
     storage.save()
-    return make_response(jsonify({}), 200)
+    return jsonify({}), 200
 
 
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
